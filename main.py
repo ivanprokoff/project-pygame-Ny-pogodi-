d21ed57c043c4e98b4291d9eadaecb8029864aac
ivanprokoff <ivanprokoff@yandex.ru>
2021-01-08 14:23:01 +0500

    chickens, start screen and background added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import os
+from loading import load_image
+from chickens import Chickens
 
 
 FPS = 50
@@ -11,21 +13,11 @@ def terminate():
     sys.exit()
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join(name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
-
-
 def start_screen():
     intro_text = ['Нажмите любую клавишу',
                   'для продолжения']
 
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    fon = pygame.transform.scale(load_image('fon.png'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
@@ -45,6 +37,9 @@ def start_screen():
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 print('поихали')
+                screen.fill(pygame.Color('white'))
+                background = pygame.transform.scale(load_image('background.png'), (width, height))
+                screen.blit(background, (0, 0))
                 return
         pygame.display.flip()
         clock.tick(FPS)
@@ -54,10 +49,15 @@ if __name__ == '__main__':
     pygame.init()
     start = True
     pygame.display.set_caption('Ну, погоди! | Помоги волку собрать яйца')
-    size = width, height = 800, 600
+    size = width, height = 600, 450
     screen = pygame.display.set_mode(size)
     running = True
     clock = pygame.time.Clock()
+    background = load_image('background.png')
+    wolf = load_image('wolf_main_left.png')
+    chickens = load_image('chickens.png')
+    all_sprites = pygame.sprite.Group()
+    Chickens(all_sprites)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -65,5 +65,6 @@ if __name__ == '__main__':
             elif start:
                 start_screen()
                 start = False
+        all_sprites.draw(screen)
         pygame.display.flip()
     pygame.quit()
